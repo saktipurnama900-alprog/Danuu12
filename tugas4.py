327f5761585301541471fd1108d3ce57d18657c7
saktipurnama900-alprog <saktipurnama900@gmail.com>
2026-01-06 00:02:09 +0800

    Add files via upload

---
+++ tugas4.py
@@ -0,0 +1,22 @@
+# mengambil inputan dari user 
+
+#f input("masukan nama anda : ")
+#print("selamat datang ", f)
+#print(type(f))
+
+#i = int(input("masukan umur anda : "))
+#print(umur anda sekarang", i)
+#print(type(i))
+
+#n =bool(input("apakah kamu selingkuh? :"))
+#print("kejujuranmu", n)
+#print(type(n))
+
+# TUGAS 
+
+nama = input ("masukan nama anda : ")
+nim = int(input("masukan nim saya : "))
+print("nama saya", nama)
+print("Nim saya", nim)
+print(type(nama))
+print(type(nim))
