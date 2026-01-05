b56f5576381ee13cd7afed48f46dc731b7d2b11b
saktipurnama900-alprog <saktipurnama900@gmail.com>
2026-01-06 00:01:52 +0800

    Add files via upload

---
+++ tugas3.py
@@ -0,0 +1,17 @@
+# tipe data 
+
+f = 6 # bilangan bulat ( integer "int")
+print("nilai dari f :", f)
+print(type(f))
+
+i = 2.2 # bilangan pecahan (float "float")
+print("nilai i :", i)
+print(type(i))
+
+n = "februari" # tipe data string (tulisan "str")
+print("nilai dari n :", n)
+print(type(n))
+
+a = False # tipe data benar salah (true/false "bool")
+print("nilai dari kebenaran : ", a)
+print(type(a))
