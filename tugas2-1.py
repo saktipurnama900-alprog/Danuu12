ebdb0c47cb21f56b2f5e265ef043fe8e42d9b269
saktipurnama900-alprog <saktipurnama900@gmail.com>
2026-01-06 00:01:37 +0800

    Add files via upload

---
+++ tugas2-1.py
@@ -0,0 +1,27 @@
+# belajar variabel
+# cara kita menyimpan nilai
+
+# memberikan nilai ( assigment )
+x = 10
+y = 5
+lebar = 12
+
+print("nilai dari x =" , x)
+print("nilai dari y =" , y)
+print("nilai dari lebar =" , lebar)
+
+# things to consider when making a variabel
+# hal hal yang harus diperhatikan ketika membuat variabel
+
+bet99 = 9 # tidak boleh diawali dengan angka 
+jumlah_hutang = 10000 # menggunakan dua suku kata, harus digabungkan dengan _
+jumlahhutang = 10000 # diagbung harus dengan huruf kapital
+
+# pemanggil indirect 
+z = lebar
+print ("nilai z adalah =", z)
+
+# TUGAS 
+nim = 25241099
+print("nama saya adalah = danu saputra")
+print("nim saya adalah = ", nim)
