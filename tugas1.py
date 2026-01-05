59dde472955c9b89b2e1b8322863f15442df4b3a
saktipurnama900-alprog <saktipurnama900@gmail.com>
2026-01-06 00:01:17 +0800

    Add files via upload

---
+++ tugas1.py
@@ -0,0 +1,18 @@
+# saya sedang belajar phyton
+# coba dibaca dari kiri ke kanan
+# coba dibaca dari atas ke bawah
+
+# cara untuk memindahkan satu baris
+# arahkan kursor pada baris yang ingin dipindahkan
+# tekan tombol alt, lalu tekan tombol arah bawah atau atas
+
+# cara untuk mengcopy satu baris full
+# sama dengan yang diatas, namun ditambahkan tekan tombol shift 
+
+print("my name is danu")
+print("saya mahasiswa cold")
+print("tapi saya ganteng") 
+
+x = 5
+y = 6
+print(x + y)
